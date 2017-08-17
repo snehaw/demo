df8956155d160293d24ad16642c45f1a9fabed34
Sneha Wakode <sneha.provab@gmail.com>
2017-08-17 15:33:36 +0530

    modified database

--- extras/demo_db.sql
+++ extras/demo_db.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jul 18, 2017 at 09:30 AM
+-- Generation Time: Aug 17, 2017 at 12:02 PM
 -- Server version: 5.6.26
 -- PHP Version: 5.6.12
 
@@ -27,9 +27,9 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `blog` (
-  `blog_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `media_id` int(11) DEFAULT NULL,
+  `blog_id` bigint(20) NOT NULL,
+  `user_id` bigint(20) NOT NULL,
+  `media_id` bigint(20) DEFAULT NULL,
   `title` varchar(200) NOT NULL,
   `content` text,
   `slug` varchar(255) DEFAULT NULL
@@ -42,9 +42,9 @@ CREATE TABLE IF NOT EXISTS `blog` (
 --
 
 CREATE TABLE IF NOT EXISTS `media` (
-  `media_id` int(11) NOT NULL,
+  `media_id` bigint(20) NOT NULL,
   `media_type_id` tinyint(2) NOT NULL,
-  `user_id` int(11) NOT NULL,
+  `user_id` bigint(20) NOT NULL,
   `name` varchar(150) NOT NULL,
   `path` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -67,14 +67,23 @@ CREATE TABLE IF NOT EXISTS `media_type` (
 --
 
 CREATE TABLE IF NOT EXISTS `user` (
-  `user_id` int(11) NOT NULL,
+  `user_id` bigint(20) NOT NULL,
   `user_type_id` tinyint(2) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(61) NOT NULL,
   `first_name` varchar(35) DEFAULT NULL,
   `last_name` varchar(75) DEFAULT NULL,
-  `email` varchar(200) NOT NULL,
-  `phone_number` bigint(15) NOT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `phone_number` bigint(15) DEFAULT NULL,
   `address` text
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`user_id`, `user_type_id`, `username`, `password`, `first_name`, `last_name`, `email`, `phone_number`, `address`) VALUES
+(1, 2, 'test@test.com', '$2y$10$52S7OToMpLOSJggw6Q4v/e4M3oXCTEuNgwvEw92TPzXMMMvoxX9pu', NULL, NULL, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -85,7 +94,15 @@ CREATE TABLE IF NOT EXISTS `user` (
 CREATE TABLE IF NOT EXISTS `user_type` (
   `user_type_id` tinyint(2) NOT NULL,
   `type` varchar(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user_type`
+--
+
+INSERT INTO `user_type` (`user_type_id`, `type`) VALUES
+(1, 'admin'),
+(2, 'user');
 
 --
 -- Indexes for dumped tables
@@ -96,8 +113,8 @@ CREATE TABLE IF NOT EXISTS `user_type` (
 --
 ALTER TABLE `blog`
   ADD PRIMARY KEY (`blog_id`),
-  ADD KEY `user_id` (`user_id`),
-  ADD KEY `media_id` (`media_id`);
+  ADD KEY `blog_ibfk_1` (`user_id`),
+  ADD KEY `blog_ibfk_2` (`media_id`);
 
 --
 -- Indexes for table `media`
@@ -105,7 +122,7 @@ ALTER TABLE `blog`
 ALTER TABLE `media`
   ADD PRIMARY KEY (`media_id`),
   ADD KEY `media_type_id` (`media_type_id`),
-  ADD KEY `user_id` (`user_id`);
+  ADD KEY `media_ibfk_2` (`user_id`);
 
 --
 -- Indexes for table `media_type`
@@ -135,12 +152,12 @@ ALTER TABLE `user_type`
 -- AUTO_INCREMENT for table `blog`
 --
 ALTER TABLE `blog`
-  MODIFY `blog_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `blog_id` bigint(20) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `media`
 --
 ALTER TABLE `media`
-  MODIFY `media_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `media_id` bigint(20) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `media_type`
 --
@@ -150,12 +167,12 @@ ALTER TABLE `media_type`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `user_id` bigint(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `user_type`
 --
 ALTER TABLE `user_type`
-  MODIFY `user_type_id` tinyint(2) NOT NULL AUTO_INCREMENT;
+  MODIFY `user_type_id` tinyint(2) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- Constraints for dumped tables
 --
